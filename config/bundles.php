6d2db8dc33a18cee677baf0dbb73761c06a9fabc
Camille <camille.mathien@gmail.com>
2023-06-29 18:54:17 +0200

    add JWT token for API authentication

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
